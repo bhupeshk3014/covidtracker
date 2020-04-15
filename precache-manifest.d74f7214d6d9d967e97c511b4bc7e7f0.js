1af2b3615886dbcab0444d69a8062fe9a229decb
Bhupesh Kumar <bhupeshkumar003@gmail.com>
2020-04-15 19:29:50 +0530

    Updates

---
+++ precache-manifest.d74f7214d6d9d967e97c511b4bc7e7f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7508b509ca4e864b3419aeaa2027b21a",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "f2a12165bce044511932",
+    "url": "/covidtracker/static/css/main.1c9c2e77.chunk.css"
+  },
+  {
+    "revision": "f7ff7aa48f0fad75cd94",
+    "url": "/covidtracker/static/js/2.2008e414.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.2008e414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a12165bce044511932",
+    "url": "/covidtracker/static/js/main.fba573e3.chunk.js"
+  },
+  {
+    "revision": "a7f4a39d59096dfd5f40",
+    "url": "/covidtracker/static/js/runtime-main.38c917b2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/image.d7265326.png"
+  }
+]);
